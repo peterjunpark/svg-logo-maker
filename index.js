7ba3df96959c5b56ada42b5867eaad6ac6d5d697
Peter Park <peterjpark@pm.me>
2023-05-11 15:15:28 -0400

    Add validation

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 const inquirer = require("inquirer");
-const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt")
-const { createLogo, writeToFile } = require("./lib/utils.js");
+const maxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
+const {
+  makeLogo,
+  writeLogo,
+  isColorHex,
+  isColorKeyword,
+} = require("./lib/utils.js");
 
-function init() {
-  inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
-  inquirer
-    .prompt([
-      {
-        name: "shape",
-        message: "Choose the shape of your logo",
-        type: "list",
-        choices: ["Circle", "Triangle", "Square"],
-        suffix: " :",
+inquirer.registerPrompt("maxlength-input", maxLengthInputPrompt);
+console.log(
+  "\x1b[1m\x1b[93m~ SVG Logo Maker ~\n" +
+    "\x1b[33mComplete the following prompts to generate a logo.\n" +
+    "Color inputs can be either hex codes or recognized SVG color keywords."
+);
+inquirer
+  .prompt([
+    {
+      name: "shape",
+      message: "Choose the shape of your logo",
+      type: "list",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      name: "text",
+      message: "Enter text for your logo",
+      type: "maxlength-input",
+      maxLength: 3,
+    },
+    {
+      name: "textColor",
+      message: "Enter your logo's text color",
+      type: "input",
+      validate: (answer) => {
+        if (isColorHex(answer) || isColorKeyword(answer)) return true;
+        return "Unrecognized color. Try again.";
       },
-      {
-        name: "text",
-        message: "Enter text for your logo",
-        type: "maxlength-input",
-        maxLength: 3,
+    },
+    {
+      name: "bgColor",
+      message: "Enter your logo's background color",
+      type: "input",
+      validate: (answer) => {
+        if (isColorHex(answer) || isColorKeyword(answer)) return true;
+        return "Unrecognized color. Try again.";
       },
-      {
-        name: "textColor",
-        message:
-          "Enter your logo's text color",
-        type: "input",
-        suffix: " (keyword and hexadecimal values allowed) :",
-      },
-      {
-        name: "bgColor",
-        message:
-          "Enter your logo's background color",
-        type: "input",
-        suffix: " (keyword and hexadecimal values allowed) :",
-      },
-    ])
-    .then((answers) => {
-      const logo = createLogo(answers);
-      console.log(logo)
-    });
-}
-init();
+    },
+  ])
+  .then((answers) => {
+    const logo = makeLogo(answers).render();
+    writeLogo(logo);
+  });
